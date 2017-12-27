731ba751cf40b139525d5e0205e6fb91f3e8c8ae
Miciaha Ivey <iveymiciaha@gmail.com>
2017-12-27 17:34:10 +0000

    Redis client change

--- bots/sentiment.js
+++ bots/sentiment.js
@@ -7,9 +7,9 @@ const config = require("./config");
 const bot = new Twit(config);
 var url = require("url");
 var redisURL = url.parse(process.env.REDISCLOUD_URL || 'redis://rediscloud:62WwEPziRxb2ooXy@redis-13077.c12.us-east-1-4.ec2.cloud.redislabs.com:13077');
-var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+var client = redis.createClient(process.env.REDISCLOUD_URL);
 if (process.env.REDISCLOUD_URL){
-    client.auth(redisURL.auth.split(":"[1]));
+    client.auth.split(":"[1]);
 }
 
 var REDIS_KEY = 'repliedTo';
